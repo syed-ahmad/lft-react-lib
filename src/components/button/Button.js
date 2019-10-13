d149831b462f590d620d384ff4bc362b91439955
Syed Ahmad <syed-ahmad@outlook.com>
2019-10-13 22:56:25 +0100

    Fix react attribute to className

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = () => {
   return (
-    <button type="button" class="btn btn-primary">
+    <button type="button" className="btn btn-primary">
       Primary
     </button>
   );
